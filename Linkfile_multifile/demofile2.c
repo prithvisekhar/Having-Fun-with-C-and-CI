ced6e671733af729ca1df809a64bbb8c5e74db2c
prithvisekhar <prithvisekhar@gmail.com>
2020-05-15 19:05:49 +0530

    MultiFile Cprogramming

---
+++ Linkfile_multifile/demofile2.c
@@ -0,0 +1,11 @@
+#include "hello.h"
+#include <stdio.h>
+
+int main()
+{
+	int c,d;
+	c=add(1,2);
+	d=subtract(4,5);
+	printf("Add:%d\n",c);
+	printf("Subtract:%d\n",d);
+}
---
+++ Linkfile_multifile/hello.h
@@ -0,0 +1,12 @@
+
+
+#ifndef HELLO_H
+#define HELLO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int add ();
+int subtract ();
+#endif
