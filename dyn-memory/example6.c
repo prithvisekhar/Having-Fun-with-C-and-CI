653ffe63ed23aad5f6a5540b93b1539b05fb026b
prithvisekhar <prithvisekhar@gmail.com>
2020-05-21 18:24:56 +0530

    Add files via upload

---
+++ dyn-memory/example6.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+const int maxlen = 20;
+
+typedef struct {
+  int empid;
+  char ename[20];
+  double esalary;
+  int eage;    
+}employee;
+
+int main(int argc, char* argv[]) {
+  employee_t *ps;
+  ps = create(1001, "Richard", 5000, 20);
+  display(ps);
+  return 0;
+}
+employee* create(int id, char* name, double sal, int age) {
+  int namelen = strlen(name);
+  int len = namelen > maxlen ? maxlen : namelen;
+  employee *ptr = malloc(sizeof(employee));
+  ptr->empid=id;
+  strncpy(ptr->ename, name);     //what if namelen > 20
+  ptr->esalary=salary;
+  ptr->eage=age;
+  return ptr;
+}
+employee* clone(employee* psrc) {
+  employee *pnew = malloc(sizeof(employee));
+  memcpy(pnew, psrc, sizeof(employee));     
+  return pnew;
+}
+void display(const employee *pc) {
+  //print pc->empid, pc->ename, pc->esalary, pc->eage
+}
+void release(employee *ptr) {
+  free(ptr);
+}
